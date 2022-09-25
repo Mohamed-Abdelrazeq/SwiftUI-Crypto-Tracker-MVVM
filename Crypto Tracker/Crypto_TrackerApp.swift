757dd36cc45e88b95c2118fb7c453d025450cc0d
Mohamed-Abdelrazeq <62867599+Mohamed-Abdelrazeq@users.noreply.github.com>
2022-09-25 14:15:14 +0200

    Initial Commit

---
+++ Crypto Tracker/Crypto_TrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Crypto_TrackerApp.swift
+//  Crypto Tracker
+//
+//  Created by mohamed abdelrazek on 25/09/2022.
+//
+
+import SwiftUI
+
+@main
+struct Crypto_TrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
